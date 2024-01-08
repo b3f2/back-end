84fcf9270c330bec96e1980ddb12c66c785f83b9
SIM Yuri <gr033@kyonggi.ac.kr>
2024-01-08 18:50:12 +0900

    fix : 파일 수정

--- src/main/java/com/backend/api/repository/post/PostRepository.java
+++ src/main/java/com/backend/api/repository/post/PostRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
 @Repository
--- src/test/java/com/backend/api/ServiceTestSupport.java
+++ src/test/java/com/backend/api/ServiceTestSupport.java
@@ -3,6 +3,10 @@ package com.backend.api;
 import com.backend.api.jwt.TokenProvider;
 import com.backend.api.repository.course.CourseLocalRepository;
 import com.backend.api.repository.post.*;
+import com.backend.api.repository.post.CategoryRepository;
+import com.backend.api.repository.post.ImageRepository;
+import com.backend.api.repository.post.PostLikeRepository;
+import com.backend.api.repository.post.PostRepository;
 import com.backend.api.repository.course.CourseRepository;
 import com.backend.api.repository.course.CourseReviewRepository;
 import com.backend.api.repository.local.FavoriteLocalRepository;
@@ -14,6 +18,7 @@ import com.backend.api.service.auth.AuthService;
 import com.backend.api.service.course.CourseLocalService;
 import com.backend.api.service.course.CourseReviewService;
 import com.backend.api.service.course.CourseService;
+import com.backend.api.service.user.UserService;
 import com.backend.api.service.Comment.CommentService;
 import com.backend.api.service.post.PostService;
 import com.backend.api.service.Comment.CommentService;
@@ -21,7 +26,6 @@ import com.backend.api.service.post.PostService;
 import com.backend.api.service.local.FavoriteService;
 import com.backend.api.service.local.LocalReviewService;
 import com.backend.api.service.local.LocalService;
-import com.backend.api.service.user.UserService;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -50,7 +54,7 @@ public abstract class ServiceTestSupport {
     protected PostService postService;
 
     @Autowired
-    protected PostRepository postRepository;
+    protected PostRepository postRepository;;
 
     @Autowired
     protected CategoryRepository categoryRepository;
